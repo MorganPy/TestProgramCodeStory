c059c576bdd7cd9eeb080d671feecb10ab64a849
Morgan <a12549445@unet.univie.ac.at>
2026-04-01 14:15:25 +0200

    4 opens in a row, 4 closes in a row

--- TestCases.jl
+++ TestCases.jl
@@ -1,5 +1,17 @@
 
 
-#<step explain="BREAK FOR TESTING">
-println("Commit break - AFTER THIS COMMITS ARE TESTING")
+#<step explain="Opening Step 1">
+println("Step Open 1")
+#<step explain="Opening Step 2">
+println("Step Open 2")
+#<step explain="Opening Step 3">
+println("Step Open 3")
+#<step explain="Opening Step 4">
+println("Step Open 4")
+#</step>
+println("Step Close 1")
+#</step>
+println("Step Close 2")
+#</step>
+println("Step Close 3")
 #</step>
